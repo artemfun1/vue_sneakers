017bf20df019e36d86d8d517296a9de9f9889869
artemfun1 <a.o.peshkov@yandex.ru>
2024-07-11 14:06:46 +0500

    theory

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
-
 <script setup>
-
+import TestComp from './components/TestComp.vue'
 </script>
 
 <template>
-  <div>5</div>
-
-
-  
+  <div class="mb-10">
+    <TestComp title="Кроссовки Puma" :price="9500" :isAdded="true"> </TestComp>
+    </div>
+    <div class="mb-10">
+    <TestComp title="Кроссовки Puma" :price="9500" :isAdded="true"> </TestComp>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style></style>
