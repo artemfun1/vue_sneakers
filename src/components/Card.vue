a2b178c59d1838b1c9b2137735b292d379a3610b
artemfun1 <a.o.peshkov@yandex.ru>
2024-07-15 17:00:58 +0500

    doing

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+
+defineProps({
+  title: String,
+  imgUrl: String,
+  price: Number,
+  isFavorite:Boolean
+});
+
+
+</script>
+
+<template>
+    
+      <div
+        class="relative bg-white m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-2xl"
+      >
+        
+          <img class="absolute top-8 left-8" src="/like-2.svg" alt="like2" />
+          <img :src="imgUrl" alt="sneakers" />
+          <p class="mt-2">{{title}}</p>
+        
+
+        <div class="flex justify-between mt-5">
+          <div class="flex flex-col">
+            <span class="text-slate=400">Цена:</span>
+            <b>{{ price }} руб.</b>
+          </div>
+          <img src="/plus.svg" alt="plus" />
+        </div>
+      </div>
+    
+</template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,41 @@
+<template >
+  <div class="grid grid-cols-4 gap-5">
+
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      imgUrl="/sneakers/sneakers-1.jpg"
+      :price="1111"
+    />
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      imgUrl="/sneakers/sneakers-1.jpg"
+      :price="1111"
+    />
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      imgUrl="/sneakers/sneakers-1.jpg"
+      :price="1111"
+    />
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      imgUrl="/sneakers/sneakers-1.jpg"
+      :price="1111"
+    />
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      imgUrl="/sneakers/sneakers-1.jpg"
+      :price="1111"
+    />
+  </div>
+</template>
+<script setup>
+import Card from './Card.vue'
+
+
+
+
+
+</script>
+<style >
+  
+</style>
