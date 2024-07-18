d1d5163783a241961b6fe05aa7e50b6ea03dcf48
artemfun1 <a.o.peshkov@yandex.ru>
2024-07-18 10:57:00 +0500

    add fetch isFavorite

--- src/App.vue
+++ src/App.vue
@@ -7,46 +7,68 @@ import Header from './components/Header.vue'
 const items = ref([])
 
 const filters = reactive({
-sortBy:'title',
-searchQuery:''
+  sortBy: 'title',
+  searchQuery: ''
 })
 
-const onChangeSelect = e =>{
+const onChangeSelect = (e) => {
   filters.sortBy = e.target.value
 }
-const onChangeInput= e =>{
-filters.searchQuery = e.target.value
+const onChangeInput = (e) => {
+  filters.searchQuery = e.target.value
 }
 
-const fetchItems=async()=>{
+const fetchItems = async () => {
   try {
-    const params={
-      sortBy:filters.sortBy,
-      
-
+    const params = {
+      sortBy: filters.sortBy
     }
 
-    if(filters.searchQuery){
-      params.title=`*${filters.searchQuery}*`
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
     }
 
-    const {data} = await axios.get(`https://00d0e9355f119a94.mokky.dev/items`,{
+    const { data } = await axios.get(`https://00d0e9355f119a94.mokky.dev/items`, {
       params
     })
-  items.value = [...data]
+    items.value = [...data].map((obj) => ({
+      ...obj,
+      isFavorite: false,
+      isAdded: false
+    }))
   } catch (err) {
     console.log(err)
   }
 }
 
+const fetchFavorites = async () => {
+  try {
+    const { data: favorites } = await axios.get(`https://00d0e9355f119a94.mokky.dev/favorites`)
+    items.value = items.value.map((item) => {
+      const favorite = favorites.find((favorite) => favorite.id === item.id)
 
-onMounted(fetchItems)
+      if (!favorite) {
+        return item
+      }
 
-watch(filters,fetchItems)
+      return {
+        ...items,
+        isFavorite:true,
+        
 
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
 
+onMounted(async () => {
+  await fetchItems()
+  await fetchFavorites()
+})
 
-
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -58,17 +80,20 @@ watch(filters,fetchItems)
       <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold">Все кроссовки</h2>
 
-        <div class="flex gap-4">
-          <select @change="onChangeSelect"  class="py-2 px-3 border rounded-md outline-none cursor-pointer">
+        <div class="flex gap-4 max-lg:block">
+          <select
+            @change="onChangeSelect"
+            class="py-2 px-3 border rounded-md outline-none cursor-pointer"
+          >
             <option value="name">По названию</option>
             <option value="price">По цене(дешевле)</option>
             <option value="-price">По цене(дороже)</option>
           </select>
 
-          <div class="relative">
+          <div class="relative max-lg:mt-1">
             <img class="absolute left-4 top-3" src="/search.svg" alt="search" />
-            <input 
-            @input="onChangeInput" 
+            <input
+              @input="onChangeInput"
               class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
               placeholder="Поиск..."
               type="text"
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="grid grid-cols-4 gap-5 mt-8">
+  <div class="grid grid-cols-4 gap-5 mt-8 max-lg:grid-cols-2 max-lg:gap-2">
     <Card
       v-for="el in items"
       :key="el.id"
       :title="el.title"
       :imgUrl="el.imageUrl"
       :price="el.price"
-      :isAdded="false"
-      :isFavorite="false"
+      isAdded
+      isFavorite
       :onClickAdd="onClickAdd"
       :onClickFavorite="onClickFavorite"
     />
